69f0ca5f6be96b4a8cf74f01e89b1607bca3cf15
Daniel Rodriguez <danielr2397@gmail.com>
2022-02-14 18:00:52 -0500

    Create tab bottom styles

--- App.tsx
+++ App.tsx
@@ -1,7 +1,5 @@
 import { NavigationContainer } from "@react-navigation/native";
-import NativeStackNav from "./src/navigation/NativeStackNav";
 import BottomTabNav from "./src/navigation/BottomTabNav";
-import DrawerNav from "./src/navigation/DrawerNav";
 
 export default function App() {
 	return (
--- src/navigation/BottomTabNav.tsx
+++ src/navigation/BottomTabNav.tsx
@@ -1,7 +1,11 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Image } from "react-native";
+
 import AccountScreen from "../screens/AccountScreen";
 import FavoriteScreen from "../screens/FavoriteScreen";
 import PokedexScreen from "../screens/PokedexScreen";
+
+import Icon from "react-native-vector-icons/FontAwesome5";
 import { ScreenNames } from "../utilities/consts";
 
 const BottomTab = createBottomTabNavigator();
@@ -9,12 +13,39 @@ const BottomTab = createBottomTabNavigator();
 export default function BottomTabNav() {
 	return (
 		<BottomTab.Navigator initialRouteName={ScreenNames.Account}>
-			<BottomTab.Screen name={ScreenNames.Account} component={AccountScreen} />
 			<BottomTab.Screen
 				name={ScreenNames.Favorite}
 				component={FavoriteScreen}
+				options={{
+					tabBarLabel: "Favoritos",
+					tabBarIcon: ({ color, size }) => (
+						<Icon name="heart" color={color} size={size} />
+					),
+				}}
 			/>
-			<BottomTab.Screen name={ScreenNames.Pokedex} component={PokedexScreen} />
+			<BottomTab.Screen
+				name={ScreenNames.Pokedex}
+				component={PokedexScreen}
+				options={{
+					tabBarLabel: "PokeBolas",
+					tabBarIcon: () => (
+						<Image
+							source={require("../../assets/pokeball.png")}
+							style={{ height: 60, width: 60, top: -16 }}
+						/>
+					),
+				}}
+			/>
+			<BottomTab.Screen
+				name={ScreenNames.Account}
+				component={AccountScreen}
+				options={{
+					tabBarLabel: "Mi cuenta",
+					tabBarIcon: ({ color, size }) => (
+						<Icon name="user" color={color} size={size} />
+					),
+				}}
+			/>
 		</BottomTab.Navigator>
 	);
 }
