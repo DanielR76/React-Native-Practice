b068ad23a5197736dc7d5d10549873e1c7d64227
Daniel <danielr2397@gmail.com>
2022-06-05 16:24:36 -0500

    Change go back arrow

--- src/screens/Pokemon/PokemonScreen.tsx
+++ src/screens/Pokemon/PokemonScreen.tsx
@@ -1,21 +1,37 @@
 import { useState, useEffect } from "react";
-import { ScrollView } from "react-native";
+import { ScrollView, View } from "react-native";
 import { useNavigation, useRoute } from "@react-navigation/core";
-// import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import type { RouteProp } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/FontAwesome5";
 
 import { HeaderPokemon, ItemPokemon, StatsPokemon } from "./components";
 import PokemonsRequest from "../../apis/getPokemons";
 
 export function PokemonScreen() {
-	// const navigation = useNavigation<NativeStackNavigationProp<any>>();
-	const route = useRoute<RouteProp<any>>();
+	const navigation = useNavigation<NativeStackNavigationProp<any>>();
+	const { params } = useRoute<RouteProp<any>>();
 	const { getPokemonById } = PokemonsRequest();
 
 	const [pokemon, setPokemon] = useState(null);
 
 	useEffect(() => {
-		getPokemonById(route.params?.id)
+		navigation.setOptions({
+			headerRight: () => <View />,
+			headerLeft: () => (
+				<Icon
+					name="arrow-left"
+					color="#fff"
+					size={20}
+					style={{ marginLeft: 20 }}
+					onPress={navigation.goBack}
+				/>
+			),
+		});
+	}, [navigation, params]);
+
+	useEffect(() => {
+		getPokemonById(params?.id)
 			.then((response) => response.name && setPokemon(response))
 			.catch(console.log);
 	}, []);
