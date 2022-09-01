2fbfdcd391f28f3ede6f546af963dd754a06d381
Daniel Rodriguez <danielr2397@gmail.com>
2022-09-01 17:06:39 -0500

    Start making profile component

--- src/screens/Account/components/User/UserData.tsx
+++ src/screens/Account/components/User/UserData.tsx
@@ -1,11 +1,34 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
+import { useAuth } from "@hooks";
+import { memo } from "react";
+
+import { ContainerStyled } from "./UserData.styled";
 
 export const UserData = () => {
+	const { authState, resetUserDispatch } = useAuth();
+	const { email, firstname, lastname, username } = authState;
+
 	return (
-		<View>
-			<Text>UserData</Text>
-		</View>
+		<ContainerStyled>
+			<View>
+				<Text>Nombre</Text>
+				<Text>{firstname + " " + lastname}</Text>
+			</View>
+			<View>
+				<Text>Usuario</Text>
+				<Text>{username}</Text>
+			</View>
+			<View>
+				<Text>Correo</Text>
+				<Text>{email}</Text>
+			</View>
+			<View>
+				<Text>Total Favoritos</Text>
+				<Text>0</Text>
+			</View>
+			<Button title="Cerrar sesion" onPress={resetUserDispatch} />
+		</ContainerStyled>
 	);
 };
 
-export default UserData;
+export default memo(UserData);
