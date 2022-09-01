19d686796206dc48292a24c1be2c256768ce0397
Daniel Rodriguez <danielr2397@gmail.com>
2022-09-01 16:32:14 -0500

    Implement aliases path

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,23 @@ module.exports = function (api) {
 	api.cache(true);
 	return {
 		presets: ["babel-preset-expo"],
-		// plugins: ["react-native-reanimated/plugin"],
+		plugins: [
+			[
+				"module-resolver",
+				{
+					root: ["./src"],
+					extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+					alias: {
+						"@apis": "./src/apis",
+						"@components": "./src/components",
+						"@context": "./src/context",
+						"@hooks": "./src/hooks",
+						"@navigation": "./src/navigation",
+						"@screen": "./src/screen",
+						"@utilities": "./src/utilities",
+					},
+				},
+			],
+		],
 	};
 };
